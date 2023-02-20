7fd51287317c17f598cc5269f7a48fc65dbdaaac
Eugene Myasnikov <justatrade@mail.ru>
2023-02-20 14:03:51 +0300

    Fixed None in generator

--- for_dict_challenges_bonus.py
+++ for_dict_challenges_bonus.py
@@ -48,7 +48,9 @@ def generate_chat_history():
             "id": uuid.uuid4(),
             "sent_at": sent_at,
             "sent_by": random.choice(users_ids),
-            "reply_for": random.choice([None, random.choice([m["id"] for m in messages]) if messages else []]),
+            "reply_for": random.choice([None,
+                                        random.choice([m["id"] for m in messages])
+                                        if messages else None]),
             "seen_by": random.sample(users_ids, random.randint(1, len(users_ids))),
             "text": lorem.sentence(),
         })
